e6bdb64a1114f2cfcff3adeaffe231f1d2c890a3
grahamjantz <grahammrjantz@gmail.com>
2022-10-25 11:43:41 -0600

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+import App from './App'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+describe('render App component', () => {
+  test('render App', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  })
+})
